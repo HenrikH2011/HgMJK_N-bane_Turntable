04b4a50db3c48abda0040b158e9b598b51550a1d
Henrik Hansen <henrik@webground.dk>
2025-03-12 13:03:06 +0100

    file update - main.cpp
    Building main.cpp - base code

--- PlatformIO/HgMJK_Turntable/src/main.cpp
+++ PlatformIO/HgMJK_Turntable/src/main.cpp
@@ -29,19 +29,40 @@ ToDo:
 
 
 // define variables
+/* ------------------- Stepper Motor Control Interface ---------------- */
+int speed_CW = 500;   // speed clockwise
+int speed CCW = -500; // speed counter clokwise
+long stepM1_steps_CW = 1000; // steps to move stepM 
+
 int delay_1 = 1000; // delay in milliseconds
 int delay_2 = 2000;
+
+
 
 
 // define Pin constants
 const int potMeter_01 = A0; // Potentiometer Pin
 
 const int LED_01 = 2; // LED Pin
-const int LED_01 = 3;
+const int LED_02 = 3;
 
 const int stepM1_IN1 = 8;  // StepMotor Pin
 const int stepM1_IN2 = 9;
 const int stepM1_IN3 = 10;
 const int stepM1_IN4 = 11;
 
+// define stepper motor interface type - 4 == FULL4WIRE
+#define motorInterfaceType_1 4 // stepM1 setup
+
+Accelstepper stepM1 = Accelstepper(MotorInterfaceType_1, stepM1_IN1, stepM1_IN3, stepM1_IN2, stepM1_IN4);
+
+// move stepM ClockWise a speed from potentiometer reading
+void moveStepM_CW (){
+  Serial.println("Move CW");
+  digitalWrite(LED_01, HIGH);
+  digitalWrite(LED_02, LOW);
+  stepM1.enableOutputs(); // enable all stepM output pins
+
+  
+}
 
