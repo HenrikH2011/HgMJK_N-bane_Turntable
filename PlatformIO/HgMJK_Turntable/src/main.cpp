44275353a09ff08df10b124d2efe63fe99ffbc5f
Henrik Hansen <henrik@webground.dk>
2025-03-12 13:35:18 +0100

    file update - main.cpp
    building main.cpp - base code - test digitalRead potmeter A0

--- PlatformIO/HgMJK_Turntable/src/main.cpp
+++ PlatformIO/HgMJK_Turntable/src/main.cpp
@@ -25,13 +25,13 @@ ToDo:
 
 // Include libraryes
 #include <Arduino.h>
-#include <accelstepper.h>
+#include <AccelStepper.h>
 
 
 // define variables
 /* ------------------- Stepper Motor Control Interface ---------------- */
 int speed_CW = 500;   // speed clockwise
-int speed CCW = -500; // speed counter clokwise
+int speed_CCW = -500; // speed counter clokwise
 long stepM1_steps_CW = 1000; // steps to move stepM 
 
 int delay_1 = 1000; // delay in milliseconds
@@ -52,17 +52,63 @@ const int stepM1_IN3 = 10;
 const int stepM1_IN4 = 11;
 
 // define stepper motor interface type - 4 == FULL4WIRE
-#define motorInterfaceType_1 4 // stepM1 setup
+#define MotorInterfaceType_1 4 // stepM1 setup
 
-Accelstepper stepM1 = Accelstepper(MotorInterfaceType_1, stepM1_IN1, stepM1_IN3, stepM1_IN2, stepM1_IN4);
+AccelStepper stepM1 = AccelStepper(MotorInterfaceType_1, stepM1_IN1, stepM1_IN3, stepM1_IN2, stepM1_IN4);
 
 // move stepM ClockWise a speed from potentiometer reading
-void moveStepM_CW (){
-  Serial.println("Move CW");
+void moveStepM (){
+  Serial.println("Move stepM CW or CCW");
   digitalWrite(LED_01, HIGH);
   digitalWrite(LED_02, LOW);
   stepM1.enableOutputs(); // enable all stepM output pins
 
+  stepM1.setCurrentPosition(0);
+
+  while ( (digitalRead(potMeter_01) < 510) && (digitalRead(potMeter_01) > 514) ) {
+    if (digitalRead(potMeter_01) < 510){
+      Serial.println("move stepM CCW");
+      stepM1.setSpeed(digitalRead(potMeter_01));
+      stepM1.runSpeed();
+    } // END if
+
+    if (digitalRead(potMeter_01) >514){
+      Serial.println("move stepM CW");
+      stepM1.setSpeed(digitalRead(potMeter_01));
+      stepM1.runSpeed();
+    } // END if
+ 
+  } // END while
+
+  stepM1.disableOutputs(); // disable all stepM output pins
+  digitalWrite(LED_01, LOW);
+  digitalWrite(LED_02, HIGH);
   
-}
+} // END void moveStepM
+
+void setup(){
+  Serial.begin(9600);
+
+  stepM1.disableOutputs();
+  stepM1.setMaxSpeed(500);
+  stepM1.setAcceleration(50);
+  
+  pinMode (LED_01, OUTPUT);
+  pinMode (LED_02, OUTPUT);
+
+  pinMode (stepM1_IN1, OUTPUT);
+  pinMode (stepM1_IN2, OUTPUT);
+  pinMode (stepM1_IN3, OUTPUT);
+  pinMode (stepM1_IN4, OUTPUT);
+
+} // END vois setup
+
+void loop(){
+
+  Serial.print("potM value: ");
+  Serial.println(digitalRead(potMeter_01));
+  delay(delay_1);
+  
+
+} // END void loop
 
